c2f744c161c862805fae1ba33dd601961182970b
Atsushi Yasuda <atsushi.yasuda.jp@gmail.com>
2015-11-23 20:03:07 +0900

    Add TaskList Component

---
+++ components/task_list.vue
@@ -0,0 +1,14 @@
+<!-- vim: set ft=html : -->
+<template>
+  <div>{{list.title}}</div>
+</template>
+
+<script>
+var TaskList = require('../model/task_list').default
+
+export default {
+  props: {
+    list: { type: Object, default: () => { return new TaskList } }
+  }
+}
+</script>
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var Vue = require('vue')
 var TaskList = require('./model/task_list').default
-var Task = require('./model/task').default
 
 var task_list = global.task_list = new TaskList
 
 new Vue({
   el: '#app',
-  data: task_list,
+  data: {
+    list: task_list,
+  },
   components: {
-    MyMessage: require('./components/message.vue'),
+    MyTaskList: require('./components/task_list.vue')
   }
 })
